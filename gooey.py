29e18979fa96712c32f7e7d799b1ad05205738f7
esquejoethanol <ethanaelgarth.esquejo.cvt@eac.edu.ph>
2025-05-11 02:11:52 +0800

    update1

--- gooey.py
+++ gooey.py
@@ -1,6 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 
-root tk.Tk()
+root = tk.Tk()
+root.tk.call("source")
+root.tk.call("source")
+style.theme_use()
+
+frame = ttk.Frame(root)
+frame.pack()
+
+widgets_frame = ttk.LabelFrame(frame, text="insert Row")
+widgets_frane.grid(row=0, column=0)
+
+name_entry = ttk.Entry(widgets_frame)
+name_entry.insert(0,"name")
+name_entry.grid(row=0, column=0, sticky="eastwest")
+
 
 root.mainloop()
